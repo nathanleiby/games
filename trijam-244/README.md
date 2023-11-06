507e02ae61f4869f3e7b53d5514c97973301ff7a
Nathan Leiby <nathanleiby@gmail.com>
2023-11-06 01:18:03 -0800

    add audio, gameplay tweaks

--- trijam-244/README.md
+++ trijam-244/README.md
@@ -4,22 +4,37 @@
 - pretty
 
   - add music
-
+    - game music
+    - game over music
   - add sounds:
 
     - collision
     - ball bounce?
     - thrust
     - game over
+
     - game lobby
       - click "accept" / start
 
+  - collision screen shake
+    - https://kidscancode.org/godot_recipes/3.x/2d/screen_shake/index.html
   - add a UI theme... nicer font
   - background
     - add a parallax background
     - or a shader background
   - user nicer art for ball and blocks -- theme?
 
-- add a jump action for funz
+- add a jump action for funz and to handle bad luck layouts
+  - make it a limited resource (e.g. 3, and they recharge slowly)
 - add a collectible for points?
 - trigger start based on user action, instead
+
+---
+
+## Assets / Art
+
+- music - https://opengameart.org/content/midnight-cruiser
+- sound
+  - https://opengameart.org/content/lose-game-short-music-clip
+  - https://opengameart.org/content/crash-collision
+  -
--- trijam-244/game.gd
+++ trijam-244/game.gd
@@ -5,22 +5,27 @@ extends Node2D
 @onready var _ball := $Ball
 @onready var _blocks := $Blocks
 @onready var _timeLabel := $UI/TimeLabel
+@onready var _jumpsLabel := $UI/JumpsLabel
 @onready var _gameOverVBox := $UI/VBoxContainer
 @onready var _gameOverOverlay := $UI/GameOverOverlay
+@onready var _collideSound := $CollideSound
 
 @onready var start_time := Time.get_ticks_msec()
+
+const MAX_JUMP_COUNT := 3
+@onready var jumpCount := MAX_JUMP_COUNT
 
 # key game-play toggles
 const SCROLL_RATE := 1.5
 const SPAWN_RATE_S := 2.2
-const force_multiplier := 150 # TODO: explore movement
-
-
+const roll_force := 150
+const jump_force := 500
 
 const RIGHT_PX := 1152
 const BOTTOM_PX := 648
 const BLOCK_WIDTH := 256
 const BLOCK_HEIGHT := 32
+
 func _ready():
 	# needed after restarting scene
 	get_tree().paused = false
@@ -29,9 +34,16 @@ func _ready():
 	
 func get_input():
 	if Input.is_action_pressed("move_right"):
-		_ball.apply_force(Vector2.RIGHT * force_multiplier)
+		_ball.apply_force(Vector2.RIGHT * roll_force)
 	elif Input.is_action_pressed("move_left"):
-		_ball.apply_force(Vector2.LEFT * force_multiplier)
+		_ball.apply_force(Vector2.LEFT * roll_force)
+	
+	if Input.is_action_just_pressed("jump"):
+		# could be on floor or wall-jumping
+		var isCollided := len(_ball.get_colliding_bodies()) > 0
+		if jumpCount > 0 and isCollided:
+			jumpCount -= 1
+			_ball.apply_impulse(Vector2.UP * jump_force)
 
 func _process(delta):
 	get_input()
@@ -40,6 +52,7 @@ func _process(delta):
 	var minutes := seconds_elapsed_total / 60
 	var seconds := seconds_elapsed_total % 60
 	_timeLabel.text = "%d:%02d" % [minutes, seconds]
+	_jumpsLabel.text = "Jumps: %d" % [jumpCount]
 
 func _physics_process(delta):
 	# Make blocks move up
@@ -51,8 +64,9 @@ func _physics_process(delta):
 			block.queue_free()
 			# dealloc after it leaves screen .. does this happen anyway?
 		
-	# die if Ball is out of bounds (top, bottom)
-	if _ball.position.y < 0 or _ball.position.y > BOTTOM_PX:
+	# die if Ball is out of bounds (totally off screen to top or bottom)
+	# TODO: impaled on spikes for funz?
+	if _ball.position.y < (0 - 16) or _ball.position.y > BOTTOM_PX:
 		gameOver()
 
 
@@ -62,16 +76,35 @@ func gameOver():
 	_gameOverOverlay.show()
 	get_tree().paused = true	
 
+var lastBlockX := 1152/2
+
 func _on_spawn_timer_timeout():
 	# spawn new blocks
 	var newBlock := blockScene.instantiate()
-	var randX := randi_range(0 + BLOCK_WIDTH / 2,RIGHT_PX - BLOCK_WIDTH /2 ) 
+	var randX := lastBlockX
+	
+	# is too close?
+	while abs(randX - lastBlockX) < 128:
+		randX = randi_range(0 + BLOCK_WIDTH / 2,RIGHT_PX - BLOCK_WIDTH /2) 
+		
+	lastBlockX = randX
 	newBlock.position = Vector2(randX, BOTTOM_PX + BLOCK_HEIGHT)
 	
 	_blocks.add_child(newBlock)
-	
 
 
 func _on_restart_button_pressed():
 	get_tree().reload_current_scene()
 	
+
+func _on_ball_body_entered(body):
+	# TODO: Not sure this is working to randomize pitch
+	_collideSound.pitch_scale  = randf_range(0.5, 1.5)
+	_collideSound.play()
+	
+	# TODO: screen shake
+
+func _on_jump_recharge_timer_timeout():
+	# TODO: collect item instead?
+	# TODO: show a proress timer so user understands how they recharge
+	jumpCount = clamp(jumpCount + 1, 0, MAX_JUMP_COUNT)
