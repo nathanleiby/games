a5eddefd6dee678641b59a7017c9ff2a4becf525
Nathan Leiby <nathanleiby@gmail.com>
2023-11-13 01:53:06 -0800

    fixup to work on itch.io

---
+++ trijam-245/vite.config.js
@@ -0,0 +1,9 @@
+// https://vitejs.dev/config/
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // Use relative path to work on Itch.io
+  // see: github.com/vitejs/vite/discussions/5081
+  base: "./",
+});
