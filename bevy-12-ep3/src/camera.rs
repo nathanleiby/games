22389bb0db848368e54d6a636efa75e5a5a134b0
Nathan Leiby <nathanleiby@gmail.com>
2023-11-28 19:11:22 -0800

    ep3: remove move_camera

--- bevy-12-ep3/src/camera.rs
+++ bevy-12-ep3/src/camera.rs
@@ -5,8 +5,7 @@ const CAMERA_DISTANCE: f32 = 80.;
 pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera)
-            .add_systems(Update, move_camera);
+        app.add_systems(Startup, spawn_camera);
     }
 }
 
@@ -15,14 +14,4 @@ fn spawn_camera(mut commands: Commands) {
         transform: Transform::from_xyz(0.0, CAMERA_DISTANCE, 0.).looking_at(Vec3::ZERO, Vec3::Z),
         ..default()
     });
-}
-
-// not part of the tutorial
-fn move_camera(mut query: Query<(Entity, &Camera, &mut Transform)>, time: Res<Time>) {
-    for (entity, camera, mut transform) in query.iter_mut() {
-        // zoom in and out
-        transform.translation.y =
-            CAMERA_DISTANCE + time.elapsed_seconds().sin() * CAMERA_DISTANCE / 2.;
-        info!("{:?} -- {:?} -- {:?}", entity, camera, transform);
-    }
 }
